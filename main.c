7b56789d5720bcedfd7917208065ba3be4a4126f
Ricardo Alonzo Ugalde <ricardo.alonzo.ugalde@gmail.com>
2020-05-12 20:23:25 -0700

    new version

--- main.c
+++ main.c
@@ -1,42 +1,82 @@
-/*
-Week 6 Assigment
-*/
-
 #include <stdio.h>
-#include <string.h>
 
-// function to swap the values at two pointers
-
-void character_swap(char* x, char* y)
+/*
+	Question 1: Print all elemnts of array except self w / recursion, w/o division
+*/
+int arrayProduct(int array_A[], int i, int leftward_sequence, int n)
 {
-	char temp = x;
-	x = y;
-	y = temp;
+	int zero = array_A[5];
+	zero = 0; // array element 5 prints set garbage value to 0
+	int current_array_status;
+	int rightward_sequence;
+	int final_array_status;
+
+	current_array_status = array_A[i]; // displays the original array values
+	if (i == n) // base case
+	{
+		return 1;
+
+	}
+	rightward_sequence = arrayProduct(array_A, i + 1, leftward_sequence * array_A[i], n); //  product to the right of the index
+	array_A[i] = leftward_sequence * rightward_sequence; // replace element [i] with product of left and right sequence
+	final_array_status = current_array_status * rightward_sequence;
+	//printf("%d\t", array_A[i]); // 
+
+	return final_array_status;
 }
 
-// Function to print all permutations of a string
-
-void permutation_print(char *a, int b, int c)
+/*
+Question 2: Recursive power function w/ negative exponents
+*/
+int recursive_power(int base, int e)
 {
-	if (b == c)
+	if (e == 0) // base case    
 	{
-		printf("%s\n", a);
+		return 1;
 	}
-	else
+
+	else if (e >= 0)
 	{
-		for (int i = 1; i <= c; i++)
-		{
-			character_swap((a + b), (a + i));
-			permutation_print(a, b + i, c);
-			character_swap((a + b), (a + i));
-		}
+		return (base * recursive_power(base, e - 1)); //recursive case for e>=0
 	}
+
+	else if (e < 0)
+	{
+		return 1 / (recursive_power(base, -e)); // recursive case for e<0
+	}
+
 }
 
-int main()
+// main function
+int main(void)
 {
-	char str[] = "ABC";
-	int n = strlen(str);
-	permutation_print(str, 0, n - 1);
+	/*
+	Quuestion 1
+	*/
+	int array_A[] = { 1,2,3,4,5 };
+	int n = 5;
+
+	arrayProduct(array_A, 0, 1, n);  //recursive function call
+	printf("\nThe final array status is as follows:\n");
+	for (int i = 0; i < n; i++)
+	{
+		printf("%d\t\n\n", array_A[i]);
+	}
+
+	/*
+	Quuestion 2
+	*/
+	int base;
+	int e;
+	int answer;
+
+	printf("Hello there! Please enter a base number:\n ");
+	scanf_s("%d", &base);
+	printf("Great! Now enter the value of the power:\n ");
+	scanf_s("%d", &e);
+	answer = recursive_power(base, e);
+	printf("%d to the power of %d is %d", base, e, answer);
+
+
 	return 0;
 }
